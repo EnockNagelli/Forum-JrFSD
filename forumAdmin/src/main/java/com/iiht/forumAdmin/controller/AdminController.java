8377cae8db9c90517454bcffc1cc1d158a3f1c3f
Enock <iihtenock@gmail.com>
2020-07-30 11:04:07 +0530

    Updated Forum Admin Service

--- forumAdmin/src/main/java/com/iiht/forumAdmin/controller/AdminController.java
+++ forumAdmin/src/main/java/com/iiht/forumAdmin/controller/AdminController.java
@@ -11,35 +11,49 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.client.RestTemplate;
 
 import com.iiht.forumAdmin.dto.CategoryDto;
 import com.iiht.forumAdmin.service.CategoryService;
+//import com.netflix.discovery.EurekaClient;
 
 @RestController
-public class AdminController 																	// PORT: 8091
+public class AdminController
 {
 	@Autowired
 	private CategoryService categoryService;
+	
+//	@Autowired
+//	private RestTemplate restTemplate;
 
-	//-------------------------------------------------------------------------------------------------------
+//	@Autowired
+//  private EurekaClient eurekaClient;
+	
+	//---------------------------------------------------------------------------------------------------
 	// SERVICE OPERATIONS
-	//-------------------------------------------------------------------------------------------------------
-	@RequestMapping (value = "/")																// 1. WORKING
- 	public String home () {
- 		return "Forum Admin application";
+	//---------------------------------------------------------------------------------------------------
+	@RequestMapping (value = "/", method = RequestMethod.GET)								// 1. WORKING
+ 	public String landingPage() {
+ 		return "Welcome to Forum Application - Admin Service.";
  	}
-	//-------------------------------------------------------------------------------------------------------
-	@PostMapping(value="/postCategory")															// 2. WORKING	
-	public ResponseEntity<Boolean> saveUpdate(@RequestBody CategoryDto categoryDto) {
+	//---------------------------------------------------------------------------------------------------
+	@RequestMapping (value = "/admin", method = RequestMethod.GET)							// 2. WORKING
+ 	public String homePage() {
+ 		return "Welcome to Forum Application - Admin Service : This is an Admin privilege to add 'Categories'.";
+ 	}
+	//---------------------------------------------------------------------------------------------------
+	@PostMapping(value="/admin/addCategory")												// 3. WORKING	
+	public ResponseEntity<Boolean> addNewCategory(@RequestBody CategoryDto categoryDto) {
 		Boolean value = categoryService.addCategory(categoryDto);
 		if (value) {
 			return new ResponseEntity<Boolean>(value, HttpStatus.OK);
 		}
 		return new ResponseEntity<Boolean>(value, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	//-------------------------------------------------------------------------------------------------------
-	@DeleteMapping(value = "/deleteCategory/{id}")												// 3. WORKING
+	//---------------------------------------------------------------------------------------------------
+	@DeleteMapping(value = "/admin/deleteCategory/{id}")									// 4. WORKING
 	public ResponseEntity<Boolean> deleteCategory(@PathVariable String id) {
 		Boolean value = categoryService.deleteCategory(id);
 		if (value) {
@@ -47,22 +61,22 @@ public class AdminController 																	// PORT: 8091
 		}
 		return new ResponseEntity<Boolean>(value, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
-	//-------------------------------------------------------------------------------------------------------
-	@GetMapping(value = "/getCategoryById/{categoryId}")										// 4. WORKING
+	//---------------------------------------------------------------------------------------------------
+	@GetMapping(value = "/admin/getCategoryById/{categoryId}")								// 5. WORKING
 	public ResponseEntity<CategoryDto> getCategoryById(@PathVariable String categoryId) {
 		return new ResponseEntity<CategoryDto>(categoryService.getCategoryById(categoryId), HttpStatus.OK);
 	}
-	//-------------------------------------------------------------------------------------------------------
-	@GetMapping(value = "/getAllCategories", produces = "application/json")						// 5. WORKING
+	//---------------------------------------------------------------------------------------------------
+	@GetMapping(value = "/admin/getAllCategories", produces = "application/json")			// 6. WORKING
 	public ResponseEntity<List<CategoryDto>> getAllCategories() {
 		return new ResponseEntity<List<CategoryDto>>(categoryService.getAllCategories(), HttpStatus.OK);
 	}
 	
-	//-------------------------------------------------------------------------------------------------------
+	//---------------------------------------------------------------------------------------------------
 	// SEARCH OPERATIONS
-	//-------------------------------------------------------------------------------------------------------
-	@GetMapping(value = "/search/category/{categoryId}", produces = "application/json")			// 1. WORKING
-	public ResponseEntity<CategoryDto> searchCategoryById(@PathVariable String categoryId) {
-		return new ResponseEntity<CategoryDto>(categoryService.getCategoryById(categoryId), HttpStatus.OK);
-	}	
+	//---------------------------------------------------------------------------------------------------
+//	@GetMapping(value = "/search/category/{categoryId}", produces = "application/json")		// 1. WORKING
+//	public ResponseEntity<CategoryDto> searchCategoryById(@PathVariable String categoryId) {
+//		return new ResponseEntity<CategoryDto>(categoryService.getCategoryById(categoryId), HttpStatus.OK);
+//	}
 }
