2d583b429fb9f3de42fa50ca7016fe5fda2076d7
Enock <iihtenock@gmail.com>
2020-07-30 11:05:07 +0530

    Updated Forum Eureka Server

--- forumEurekaServer/src/main/java/com/iiht/forumEurekaServer/ForumEurekaServerApplication.java
+++ forumEurekaServer/src/main/java/com/iiht/forumEurekaServer/ForumEurekaServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ForumEurekaServerApplication {									// PORT: 8761
+public class ForumEurekaServerApplication {												// PORT: 8761
 
 	public static void main(String[] args) {
 		SpringApplication.run(ForumEurekaServerApplication.class, args);
